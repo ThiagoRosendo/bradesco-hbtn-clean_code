1bb7d1ca67ffba9d91fddab9b61247c4c81d2d58
Thiago Rosendo <thiago_953@hotmail.com>
2025-10-14 21:37:26 +0000

    0. Uso da Nomenclatura

---
+++ 0x00/nomenclatura/Name.java
@@ -0,0 +1,73 @@
+import java.net.URL;
+import java.util.Date;
+import java.util.List;
+
+public class Name {
+    //B
+    public Date d; // modified date
+    public List<Testcase> find(Name.User user){
+        return null;
+    };
+
+    //B
+    public Account[] AccountList; 
+
+    //B
+    public void deleteUser(Name.User user){};
+
+    //B
+    public URL url;
+
+    // Classe possui nomes próprios, funções devem ter verbos
+    //A
+    public class TableRepresent {
+        //...
+    };
+    public void saveCar(){
+        // ...
+    };
+    //B
+    public class TableRepresenter{
+        // ...
+    };
+
+    public void car(){
+        //...
+    }; 
+
+    //B
+    public void saveUser(){
+        //...
+    }; 
+
+    public void createAccount(){
+        // ...
+    }; 
+    
+    public void generateContract(){
+        // ...
+    };
+
+
+    //Use constants, enums instead of magic numbers, abbreviations
+    //B    
+    public final int DAYS_IN_YEAR = 360;
+    public final int HOURS_IN_WEEK = 168;
+
+    // Classe interna User (assumindo que existe)
+    public static class User {
+        // implementação da classe User
+    }
+
+}
+    
+    // Classe auxiliar Account (assumindo que existe)
+    class Account {
+        // implementação da classe Account
+    }
+    
+    // Classe auxiliar Testcase (assumindo que existe)
+    class Testcase {
+        // implementação da classe Testcase
+    }
+
